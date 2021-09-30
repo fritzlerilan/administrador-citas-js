f6de005a8021e5b6432e57a9a8afdee6f4437468
fritzlerilan <fritzlerilan@gmail.com>
2021-09-30 11:42:56 -0300

    Editar cita. BugFix btnEditar

--- js/app.js
+++ js/app.js
@@ -85,8 +85,8 @@ class UI {
             btnEliminar.innerHTML = 'Eliminar &times';
 
             const btnEditar = document.createElement('button');
-            btnEliminar.classList.add('btn', 'btn-info', 'mr-2');
-            btnEliminar.innerHTML = 'Editar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>';
+            btnEditar.classList.add('btn', 'btn-info', 'mr-2');
+            btnEditar.innerHTML = 'Editar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>';
 
             btnEliminar.onclick = () => eliminarCita(id);
             btnEditar.onclick = () => cargarEdicion(cita);
@@ -159,7 +159,7 @@ function validarFormulario(e) {
         administradorCitas.agregarCita({ ...citaObj });
         ui.imprimirAlerta('Se agrego correctamente');
     }
-    
+
     reiniciarObjetoCita();
     formulario.reset();
     ui.imprimirCitas(administradorCitas);
